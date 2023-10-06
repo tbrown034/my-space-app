bd19f51c9643a785ac214332fe545ad0f99a1cb9
tbrown034 <tbrown034@gmail.com>
2023-10-06 00:27:41 -0400

    start of WIP to migrate api calls from client to server with nasa and nyt

--- app/UI/Footer.jsx
+++ app/UI/Footer.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <>
-      <>hi</>
-    </>
+    <footer className="flex items-center justify-between p-4 mt-10 text-blue-800 border-t border-blue-400">
+      <div className="flex flex-col items-center justify-center text-center border-r border-blue-400">
+        <h1 className="text-xl font-extrabold">Tech Orbit</h1>
+        <h3 className="font-semibold">Read. Learn. Discover.</h3>
+      </div>
+      <div className="pl-4 text-center">
+        <div>© 2023 Tech Orbit. All rights reserved.</div>
+      </div>
+    </footer>
   );
 };
 
